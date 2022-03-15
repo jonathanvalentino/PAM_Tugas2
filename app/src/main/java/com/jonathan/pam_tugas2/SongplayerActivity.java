93b9e1e6dcd10d2fa5b09ce9d87357d9e8254f0a
jonathanvalentino <jovalen029@gmail.com>
2022-03-15 13:02:29 +0700

    Initial commit

---
+++ app/src/main/java/com/jonathan/pam_tugas2/SongplayerActivity.java
@@ -0,0 +1,14 @@
+package com.jonathan.pam_tugas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SongplayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_songplayer);
+    }
+}
