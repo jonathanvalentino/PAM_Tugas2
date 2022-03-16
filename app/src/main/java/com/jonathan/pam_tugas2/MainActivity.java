7cb7a450a69215ffc46dd014bbc96389d13a2d02
jonathanvalentino <jovalen029@gmail.com>
2022-03-16 22:30:13 +0700

    Change MainActivity method, add items layout, custom adapter.

--- app/src/main/java/com/jonathan/pam_tugas2/MainActivity.java
+++ app/src/main/java/com/jonathan/pam_tugas2/MainActivity.java
@@ -13,31 +13,39 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MY_PERMISSION_REQUEST = 1;
 
-    ArrayList<String> arrayList;
     ListView listView;
+    String[] items;
 
-    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavBar);
 
@@ -70,41 +78,45 @@ public class MainActivity extends AppCompatActivity {
                         new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST);
             }
         } else{
-            doStuff();
+            displaySong();
         }
+
+
     }
 
-    public void doStuff() {
-        listView = (ListView) findViewById(R.id.listView);
-        arrayList = new ArrayList<>();
-        getMusic();
-        adapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, arrayList);
-        listView.setAdapter(adapter);
+    public ArrayList<File> findSong(File file) {
+        ArrayList arrayList = new ArrayList();
+        File [] files = file.listFiles();
+        if(files != null) {
+            for (File singlefile : files) {
+                if (singlefile.isDirectory() && !singlefile.isHidden()) {
+                    arrayList.addAll(findSong(singlefile));
+                } else {
+                    if (singlefile.getName().endsWith(".wav")) {
+                        arrayList.add(singlefile);
+                    } else if (singlefile.getName().endsWith(".mp3")) {
+                        arrayList.add(singlefile);
+                    }
+                }
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //open mus
             }
-        });
-    }
-
-    public void getMusic(){
-        ContentResolver contentResolver = getContentResolver();
-        Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        Cursor songCursor = contentResolver.query(songUri, null, null, null, null);
-
-        if(songCursor != null && songCursor.moveToFirst()) {
-            int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
-            int songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
-
-            do{
-                String currentTitle = songCursor.getString(songTitle);
-                String currentArtist = songCursor.getString(songArtist);
-                arrayList.add(currentTitle + "\n" + currentArtist);
-            } while(songCursor.moveToNext());
         }
+        return arrayList;
     }
+
+    public void displaySong() {
+
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+        items = new String[mySongs.size()];
+        for(int i=0; i<mySongs.size(); i++){
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+        }
+
+        customAdapter customAdapter = new customAdapter();
+        listView.setAdapter(customAdapter);
+
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -116,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
                             Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                         Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show();
 
-                        doStuff();
+                        displaySong();
                     }
                 } else {
                     Toast.makeText(this, "Permission not granted!", Toast.LENGTH_SHORT).show();
@@ -129,5 +141,30 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    class customAdapter extends BaseAdapter{
 
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.list_item, null);
+            TextView txtSong =  view.findViewById(R.id.txtSong);
+            txtSong.setSelected(true);
+            txtSong.setText(items[i]);
+            return view;
+        }
+    }
 }
--- app/src/main/java/com/jonathan/pam_tugas2/SongplayerActivity.java
+++ app/src/main/java/com/jonathan/pam_tugas2/SongplayerActivity.java
@@ -4,20 +4,51 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class SongplayerActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView songName;
+    private ImageView btnPlayPause, btnNext, btnPrevious, btnLoop, btnShuffle, btnQueue;
+    private MediaPlayer mediaPlayer;
+
+    int position;
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songplayer);
 
+        seekBar = findViewById(R.id.seekBar);
+        songName = findViewById(R.id.songName);
+        btnPlayPause = findViewById(R.id.btnPlayPause);
+        btnNext = findViewById(R.id.btnNext);
+        btnPrevious = findViewById(R.id.btnPrevious);
+        btnLoop = findViewById(R.id.btnLoop);
+        btnShuffle = findViewById(R.id.btnShuffle);
+        btnQueue = findViewById(R.id.btnQueue);
+
+
+
+
+
+
+        //Back button
         ImageView imageBack = findViewById(R.id.btnBack);
         imageBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +57,7 @@ public class SongplayerActivity extends AppCompatActivity {
             }
         });
 
+        //Navbar bawah
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavBar);
 
         bottomNavigationView.setSelectedItemId(R.id.player_menu);
@@ -37,9 +69,8 @@ public class SongplayerActivity extends AppCompatActivity {
                     case R.id.player_menu:
                         return true;
                     case R.id.home_menu:
-                        startActivity(new Intent(getApplicationContext()
-                            ,MainActivity.class));
-                        overridePendingTransition(0,0);
+                        onBackPressed();
+                        overridePendingTransition(0, 0);
                         return false;
                 }
                 return false;
